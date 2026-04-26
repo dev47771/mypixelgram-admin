2ac12be49db169d3ff3798a1fa01037cf39a3fb0
Vika <vikapetrova8@gmail.com>
2026-04-27 00:33:35 +0300

    feat: add users list table with pagination

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from 'react'
+import { Sidebar } from '@/widgets/Sidebar'
+
+function PrivatePagesLayout({ children }: { children: ReactNode }) {
+   return (
+      <div className="mx-auto w-full max-w-[1280px] px-[60px]">
+         <Sidebar />
+         <main className="ml-[162px] flex min-w-0 flex-col pt-[60px] pl-6">{children}</main>{' '}
+      </div>
+   )
+}
+export default PrivatePagesLayout
