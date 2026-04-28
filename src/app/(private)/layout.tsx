672af1e4b67df8cd3428e88e79d2e4236d96abaa
Vika <vikapetrova8@gmail.com>
2026-04-28 23:31:49 +0300

    chore: remove unnecessary whitespace

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -5,7 +5,7 @@ function PrivatePagesLayout({ children }: { children: ReactNode }) {
    return (
       <div className="mx-auto w-full max-w-[1280px] px-[60px]">
          <Sidebar />
-         <main className="ml-[162px] flex min-w-0 flex-col pt-[60px] pl-6">{children}</main>{' '}
+         <main className="ml-[162px] flex min-w-0 flex-col pt-[60px] pl-6">{children}</main>
       </div>
    )
 }
